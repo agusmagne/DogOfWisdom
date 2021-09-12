36cc8b51542fe1f0b625d5d0e216c2f03612ad10
agusmagne <agusmagne@gmail.com>
2021-09-13 00:02:14 +0100

    fix safecall. catch method was outside of scope

--- app/src/main/java/com/example/dogofwisdom/view/BaseViewModel.kt
+++ app/src/main/java/com/example/dogofwisdom/view/BaseViewModel.kt
@@ -8,80 +8,73 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 open class BaseViewModel : ViewModel() {
-    
-    /**
-     * Allows ViewModels to execute coroutines safely
-     */
-    protected fun safeCall(dispatcher: CoroutineDispatcher? = null, method: suspend () -> Unit) {
-        try {
-            if (dispatcher == null) {
-                CoroutineScope(Dispatchers.Main).launch {
-                    method.invoke()
-                }
-            } else {
-                CoroutineScope(dispatcher).launch {
-                    method.invoke()
-                }
-            }
-        } catch (e: Throwable) {
-            e.stackTrace.forEach { println(it) }
-            println(e.message)
-            BaseActivity.ErrorNotification.postEvent(R.string.server_error)
-        }
-
-    }
-    
-    /**
-     * Class implemented for single one time events with any type of content
-     */
-    class Event<out T>(private val content: T) {
-
-        private var hasBeenHandled: Boolean = false
-
-        fun getContentIfNotHandled(): T? {
-            return if (hasBeenHandled) {
-                null
-            } else {
-                hasBeenHandled = true
-                content
-            }
-        }
-    }
-    
-    /**
-     * Class implemented for single on time events, but nothings gets passed as data
-     */
-    class VoidEvent {
-
-        var hasBeenHandled: Boolean = false
-
-        fun hasBeenHandled(): Boolean {
-            return if (hasBeenHandled) {
-                true
-            } else {
-                hasBeenHandled = true
-                false
-            }
-        }
-
-    }
-
+	
+	/**
+	 * Allows ViewModels to execute coroutines safely
+	 */
+	protected fun safeCall(dispatcher: CoroutineDispatcher? = null, method: suspend () -> Unit) {
+		CoroutineScope(dispatcher ?: Dispatchers.Main).launch {
+			try {
+				method.invoke()
+			} catch (e: Throwable) {
+				e.stackTrace.forEach { println(it) }
+				println(e.message)
+				BaseActivity.ErrorNotification.postEvent(R.string.server_error)
+			}
+		}
+	}
+	
+	/**
+	 * Class implemented for single one time events with any type of content
+	 */
+	class Event<out T>(private val content: T) {
+		
+		private var hasBeenHandled: Boolean = false
+		
+		fun getContentIfNotHandled(): T? {
+			return if (hasBeenHandled) {
+				null
+			} else {
+				hasBeenHandled = true
+				content
+			}
+		}
+	}
+	
+	/**
+	 * Class implemented for single on time events, but nothings gets passed as data
+	 */
+	class VoidEvent {
+		
+		var hasBeenHandled: Boolean = false
+		
+		fun hasBeenHandled(): Boolean {
+			return if (hasBeenHandled) {
+				true
+			} else {
+				hasBeenHandled = true
+				false
+			}
+		}
+		
+	}
+	
 }
 
 fun <T> MutableLiveData<out BaseViewModel.Event<T>>.postEvent(content: T) {
-    value = BaseViewModel.Event(content)
+	value = BaseViewModel.Event(content)
 }
 
 fun <T> LiveData<out BaseViewModel.Event<T>>.observeEvent(
-    owner: LifecycleOwner,
-    onEventUnhandled: (T) -> Unit
+	owner: LifecycleOwner,
+	onEventUnhandled: (T) -> Unit
 ) {
-    observe(owner, Observer { it?.getContentIfNotHandled()?.let(onEventUnhandled) })
+	observe(owner, Observer { it?.getContentIfNotHandled()?.let(onEventUnhandled) })
 }
 
 fun LiveData<out BaseViewModel.VoidEvent>.observeEvent(
-    owner: LifecycleOwner,
-    onEventUnhandled: () -> Unit
+	owner: LifecycleOwner,
+	onEventUnhandled: () -> Unit
 ) {
-    observe(owner, Observer { if (!it.hasBeenHandled()) onEventUnhandled() })
+	observe(owner, Observer { if (!it.hasBeenHandled()) onEventUnhandled() })
 }
